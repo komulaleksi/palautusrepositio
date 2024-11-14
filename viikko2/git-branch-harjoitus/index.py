7d136dd7a1709383846d706e5c29e9fbba891038
Aleksi Komulainen <aleksi.komulainen1@gmail.com>
2024-11-14 18:33:03 +0200

    Update index.py

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -6,3 +6,16 @@ x = int(input("luku 1: "))
 y = int(input("luku 2: "))
 
 logger("lopetetaan")
+# tehdään alussa importit
+
+from logger import logger
+from summa import summa
+from erotus import erotus
+
+logger("aloitetaan")
+
+x = int(input("luku 1: "))
+y = int(input("luku 2: "))
+print(f"{summa(x, y)}")
+print(f"{erotus(x, y)}")
+
