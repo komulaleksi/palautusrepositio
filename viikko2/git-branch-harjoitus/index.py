cdc874d7cdf85b3c15fc025f7e441fd27450f930
Aleksi Komulainen <aleksi.komulainen1@gmail.com>
2024-11-14 18:36:37 +0200

    bugikorjaus

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -1,20 +1,10 @@
-from logger import logger
-
-logger("aloitetaan")
-
-x = int(input("luku 1: "))
-y = int(input("luku 2: "))
-
-logger("lopetetaan")
 # tehdään alussa importit
 
 from logger import logger
 from summa import summa
 from erotus import erotus
 
-logger("aloitetaan")
-
-x = int(input("luku 1: "))
+logger("aloitetaan ohjelma") # muutos mainissax = int(input("luku 1: "))
 y = int(input("luku 2: "))
 print(f"{summa(x, y)}")
 print(f"{erotus(x, y)}")
